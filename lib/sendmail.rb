920eb9b0244212929225e5c9af0f5752f929ad4d
Benoit MARTIN-CHAVE <benoitmartinchave@gmail.com>
2014-03-13 13:43:38 +0100

    Add args

--- lib/sendmail.rb
+++ lib/sendmail.rb
@@ -1,14 +1,18 @@
 module Sendmail
   class << self
-    attr_accessor :bin
+    attr_accessor :bin, :args
     
     def bin
       @bin ||= `which sendmail`.chomp!
     end
+    
+    def args
+      @args ||= "-i"
+    end
   end
   
   def sendmail!(to, mail)
-    io = IO.popen([Sendmail.bin, to], "w+b")
+    io = IO.popen([Sendmail.bin, Sendmail.args, to], "w+b")
     io.write(mail)
     io.close
     $?.to_i == 0
